225973d468b38fcd4f1532e4108acfe7a78f8f47
hchend2 <hervechendjou6@gmail.com>
2025-04-21 19:24:07 -0500

    html and css edited to modify the webpage and some javascript added

--- helloworld-javascript.js
+++ helloworld-javascript.js
@@ -1,3 +1,34 @@
-    <script>
-        // This is a simple JavaScript program that prints "Hello, World!" to the console
-    </script>
+
+// by default, the div is hidden
+document.getElementById("myDiv3").style.display = "none";
+
+// Function to show the div when the button is clicked
+// and hide it when clicked again
+// and fade in the div
+var myVar = document.getElementById("myButton");
+
+myVar.onclick = function() {
+    var thisDiv = document.getElementById("myDiv3");
+    if (thisDiv.style.display === "none") {
+        fadeIn(thisDiv);
+        thisDiv.style.display = "block";
+    }
+    else {
+        thisDiv.style.display = "none";
+        
+    }
+}
+
+// Function to fade in an element
+function fadeIn(thisEl) {
+    thisEl.style.opacity = 0;
+    thisEl.style.transition = "opacity 0.5s ease-in-out";
+    setInterval(function() {
+        var opacity = thisEl.style.opacity;
+        if (opacity < 1) {
+            opacity += 0.9;
+            thisEl.style.opacity = opacity;
+        } else {
+            clearInterval(this);
+        }
+    }, 255);}
